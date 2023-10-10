29ab2fd590fae9090bfed544c6dff9dcb654dd11
Jialiang Chen <jialiang.chen@estudiants.urv.cat>
2023-10-10 20:40:38 +0200

    Added constants and separated calculations into a function

--- main.cpp
+++ main.cpp
@@ -1,32 +1,38 @@
 #include "mbed.h"
 #include "ThisThread.h"
 
-#define ADCres 65535.0
+#define ADCRES 65535.0
+#define R 100000.0
+#define BETA 4250
+#define K 273.15
+#define T0 298.15
 
 // main() runs in its own thread in the OS
 
 AnalogIn temp_sensor (A0);
 
-int main()
-{
+float readTemperature(){
     float thermR=NULL;
     float rawTemp=NULL;
     float tempK=NULL;
-    float tempC=NULL;
-    while(true){
-        rawTemp = temp_sensor.read_u16();
-        printf ("Raw temp = %f\n", rawTemp);
 
-        thermR = 100000*((65535.0/rawTemp)-1);
-        printf ("Thermistor Resistance = %f\n", thermR);
+    rawTemp = temp_sensor.read_u16();
+    printf ("Raw temp = %f\n", rawTemp);
 
-        tempK = 1/(log(thermR/100000.0)/4250+(1.0/298.15));
-        
-        tempC = tempK - 273.15;
-        printf("Temperature in kelvin: %f\nTemperature in Celcius: %f\n\n", tempK, tempC);
+    thermR = R*((ADCRES/rawTemp)-1);
+    printf ("Thermistor Resistance = %f\n", thermR);
 
-        ThisThread::sleep_for(1000ms);
-    }
+    tempK = 1/(log(thermR/R)/BETA+(1.0/T0));
+    printf("Temperature in kelvin: %f\n, tempK");
     
+    return (tempK - K);
+
 }
 
+int main()
+{
+    while(true){
+        printf("Temperature in Celcius: %f\n\n",readTemperature());
+        ThisThread::sleep_for(1000ms);
+    }
+}
