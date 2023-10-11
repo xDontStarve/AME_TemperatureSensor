7c22475befbdd260d0b7fec63d6d7f46e3ba621e
Adrian Izquierdo <adrian.izquierdo@estudiants.urv.cat>
2023-10-11 18:53:48 +0200

    Library Created

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "mbed.h"
 #include "ThisThread.h"
+#include "TempSensor.h"
 
 #define ADCRES 65535.0
 #define R 100000.0
@@ -8,10 +9,39 @@
 #define T0 298.15
 
 // main() runs in its own thread in the OS
-
 AnalogIn temp_sensor (A0);
 
-float readTemperature(){
+
+//Create a class
+class TempSensor{
+    public:
+        TempSensor(PinName pin) : _pin(pin){
+            _pin = 0;
+        }
+        void readTemperature(){
+            float thermR=NULL;
+            float rawTemp=NULL;
+            float tempK=NULL;
+
+            rawTemp = temp_sensor.read_u16();
+            printf ("Raw temp = %f\n", rawTemp);
+
+            thermR = R*((ADCRES/rawTemp)-1);
+            printf ("Thermistor Resistance = %f\n", thermR);
+
+            tempK = 1/(log(thermR/R)/BETA+(1.0/T0));
+            printf("Temperature in kelvin: %f\n, tempK");
+            
+            printf("Temperature in Celcius: %f\n\n",(tempK - K));
+        }
+
+    private:
+        AnalogIn _pin;
+};
+
+TempSensor temp_sensor(A0);
+
+void readTemperature(){
     float thermR=NULL;
     float rawTemp=NULL;
     float tempK=NULL;
@@ -25,14 +55,13 @@ float readTemperature(){
     tempK = 1/(log(thermR/R)/BETA+(1.0/T0));
     printf("Temperature in kelvin: %f\n, tempK");
     
-    return (tempK - K);
+    printf("Temperature in Celcius: %f\n\n",(tempK - K));
 
 }
 
 int main()
 {
     while(true){
-        printf("Temperature in Celcius: %f\n\n",readTemperature());
-        ThisThread::sleep_for(1000ms);
+        temp_sensor.readTemperature();
     }
 }
